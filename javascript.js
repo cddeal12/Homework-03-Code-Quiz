4c2fdc8e1e705ddb0c3e7ddd4c6df4db56afde90
Nemancer <cddeal12@gmail.com>
2020-09-09 16:20:26 -0700

    Completed the change between questions when an answer is chosen

--- javascript.js
+++ javascript.js
@@ -5,7 +5,10 @@ var startButton = document.getElementById("start-button");
 var answersList = document.getElementById("answers-list");
 const emptyAnswers = document.getElementById("answers-list");
 var highscoreLink = document.getElementById("highscore-link");
+var finalHeader = document.querySelector(".final-header");
+var outroText = document.querySelector(".outro-text")
 var timerText = document.getElementById("timer");
+var timeLeft = 100;
 
 // Answer slots to be populated
 var slot1El = document.getElementById("slot1");
@@ -23,13 +26,10 @@ var question5 = {questionHeader:"Question 5 head", answer1:"q5a1", answer2:"q5a2
 
 var questions = [question1, question2, question3, question4, question5];
 
-// Runs the functions to handle the quiz when the start button is pressed
-startButton.addEventListener("click", function() {
-    runQuiz();
-});
 
-//Runs through each question in the questions variable, handling them one by one with handleQuestion()
-function runQuiz() {
+
+//Runs through each question in the questions variable, handling them one by one with handleAnswer()
+function runQuiz(i) {
     // Makes the intro-text invisible
     intro.setAttribute("class", "d-none");
 
@@ -39,26 +39,74 @@ function runQuiz() {
     // Makes the start button invisible
     startButton.setAttribute("class", "d-none")
 
-    // Runs a function that displays each question one-by-one as answers are chosen
-    for (i=0; i<questions.length; i++) {
-        handleQuestion(i);
-    };
-};
+        
+    // sets the variable currentQuestion to the question being indexed
+    var currentQuestion = questions[i];
 
-function handleQuestion(index) {
-    var currentquestion = questions[index];
-    console.log(currentquestion.questionHeader);
-    // Empties out the list element
+    // Empties out the list element to reset from previous questions
     answersList = emptyAnswers;
-    console.log("answers emptied");
 
-    // Replaces header with a question
-    header.textContent = currentquestion.questionHeader;
-    console.log("header replaced");
+    // Resets the question buttons to be incorrect, before one is assigned as correct
+    slot1El.setAttribute("class", "btn btn-primary");
+    slot2El.setAttribute("class", "btn btn-primary");
+    slot3El.setAttribute("class", "btn btn-primary");
+    slot4El.setAttribute("class", "btn btn-primary");
+
+    // Replaces header with a question's text
+    header.textContent = currentQuestion.questionHeader;
 
     // Replaces each slot in the list with the text for answers
-    slot1El.textContent = currentquestion.answer1;
-    slot2El.textContent = currentquestion.answer2;
-    slot3El.textContent = currentquestion.answer3;
-    slot4El.textContent = currentquestion.answer4;
+    slot1El.textContent = currentQuestion.answer1;
+    slot2El.textContent = currentQuestion.answer2;
+    slot3El.textContent = currentQuestion.answer3;
+    slot4El.textContent = currentQuestion.answer4;
+
+    //Assigns the 'correct' class to the button with the appropriate index
+    if (currentQuestion.correctAns === 1) {
+            slot1El.setAttribute("class", "btn btn-primary correct");
+    } else if (currentQuestion.correctAns === 2) {
+            slot2El.setAttribute("class", "btn btn-primary correct");
+    } else if (currentQuestion.correctAns === 3) {
+            slot3El.setAttribute("class", "btn btn-primary correct");
+    } else if (currentQuestion.correctAns === 4) {
+            slot4El.setAttribute("class", "btn btn-primary correct");
+    }
+
+    // Checks to see when a button is clicked, subtracts time if the one clicked is not correct
+    answersList.addEventListener("click", function(event) {
+            console.log("you clicked the list")
+            if (event.target.matches(".btn")) {
+                if (event.target.matches(".correct")) {
+                    console.log("answer chosen, correct!")
+                    if (i === questions.length -1) {
+                        lastPage();
+                    } else {
+                        runQuiz(i+1);
+                    };
+                } else {
+                    timeLeft = timeLeft - 10;
+                    console.log(timeLeft);
+                    console.log("answer chosen, wrong");
+                    if (i === questions.length - 1) {
+                        lastPage();
+                    } else {
+                        runQuiz(i+1)};
+                }
+            }
+    });
 };
+
+// Changes the page elements to display the final page
+function lastPage() {
+    finalHeader.setAttribute("class", "final-header d-block");
+    outroText.setAttribute("class", "outro-text d-block");
+    answersList.setAttribute("class", "d-none");
+    header.setAttribute("class", "d-none");
+};
+
+
+
+// Runs the functions to handle the quiz when the start button is pressed
+startButton.addEventListener("click", function() {
+    runQuiz(0);
+});
